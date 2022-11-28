d7541f6744ea355960a4c6fd223e031c3dbf33ee
Juan carlos <juan.aristizabal@and.gov.co>
2022-11-27 21:04:05 -0500

    update

--- insert_files.sh
+++ insert_files.sh
@@ -1,4 +1,4 @@
-cp ./files/COSubjectClientIdDecoder.java  ../src/common-util/src/main/java/ee/ria/xroad/common/util/COSubjectClientIdDecoder.java
+cp ./files/COSubjectClientIdDecoder.java  ../src/common-util/src/main/java/ee/ria/xroad/common/util/COLSubjectClientIdDecoder.java
 cp ./files/ColAuthCertificateProfileInfo.java ../src/common-util/src/main/java/ee/ria/xroad/common/certificateprofile/impl/ColAuthCertificateProfileInfo.java
 cp ./files/ColCertificateProfileInfoProvider.java ../src/common-util/src/main/java/ee/ria/xroad/common/certificateprofile/impl/ColCertificateProfileInfoProvider.java
 cp ./files/ColSignCertificateProfileInfo.java ../src/common-util/src/main/java/ee/ria/xroad/common/certificateprofile/impl/ColSignCertificateProfileInfo.java
